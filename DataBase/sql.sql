269d3071f69cf5861dbdcfa79ecaef725dfdf901
Chujer <113624740+Chujer@users.noreply.github.com>
2025-09-01 20:43:54 +0900

    2025.09.01 SQL 기초 문법의 이해(데이터 다루기)
    SQL 기초 문법의 이해(데이터 다루기)

--- DataBase/sql.sql
+++ DataBase/sql.sql
@@ -1,12 +1,1 @@
-CREATE DATABASE customer_db;
-
-create table customer(
-	no INTEGER NOT NULL,
-    name CHAR(20) NOT NULL,
-    age TINYINT,
-    phone VARCHAR(20),
-    email VARCHAR(30) NOT NULL,
-    address VARCHAR(50)
-    
-    primary key(no)
-)
+UPDATE students SET name = 'DAVE' WHERE id = 1;
