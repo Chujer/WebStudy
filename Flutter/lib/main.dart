34e49ecbd915022d7083940f6334b808413778d4
Chujer <113624740+Chujer@users.noreply.github.com>
2025-11-13 19:30:31 +0900

    2025_11_13 실습예제 날씨 어플

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -10,31 +10,79 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Hello Flutter",
-              style: TextStyle(
-                color: Colors.amber,
-                fontSize: 30,
-                fontStyle: FontStyle.italic,
-                backgroundColor: Colors.blue,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 5,   ///자간
-                wordSpacing: 20, /// 띄어쓰기 공백크기
-                decoration: TextDecoration.underline,
-                decorationStyle: TextDecorationStyle.wavy,
-                decorationColor: Colors.red,
+        appBar: AppBar(
+          title: Text("Weather App", style: TextStyle(color: Colors.white)),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft, end: Alignment.bottomRight,
+              colors: [Colors.blue, Colors.white]
+            )
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.wb_sunny, size: 100, color: Colors.yellow),
+                SizedBox(
+                  height: 20,
+                ),
+                Text('27℃', style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold
+                ),),
+                SizedBox(
+                  height: 30,
+                ),
+                Text('Sunny', style: TextStyle(
+                  fontSize: 24,
+                ),),
+                SizedBox(
+                  height: 18,
+                ),
+                Text('New York City, USA', style: TextStyle(
+                  fontSize: 20
+                ),),
+                SizedBox(
+                  height: 60,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Text("Humidity"),
+                        SizedBox(height: 10,),
+                        Text("50%", style: TextStyle(fontSize: 20),)
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text("Wind"),
+                        SizedBox(height: 10,),
+                        Text("50%", style: TextStyle(fontSize: 20),)
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text("Pressure"),
+                        SizedBox(height: 10,),
+                        Text("50%", style: TextStyle(fontSize: 20),)
+                      ],
+                    ),
+                  ],
+                )
 
-              ),
+              ],
             ),
-            Text("data")
-          ],
+          ),
         ),
       ),
     );
   }
 }
-
-
