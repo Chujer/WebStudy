5b22cac33961cfd294f49de88657370c85d04e95
Chujer <113624740+Chujer@users.noreply.github.com>
2025-11-13 19:04:28 +0900

    2025_11_13 Text

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -11,36 +11,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Stack(   ///Stack이란 : 겹칠 수 있는 위젯 컬럼과 로우는 겹치지않음
-            children: [
-              Container(
-                width: 400,
-                height: 400,
-                color: Colors.yellow,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Hello Flutter",
+              style: TextStyle(
+                color: Colors.amber,
+                fontSize: 30,
+                fontStyle: FontStyle.italic,
+                backgroundColor: Colors.blue,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 5,   ///자간
+                wordSpacing: 20, /// 띄어쓰기 공백크기
+                decoration: TextDecoration.underline,
+                decorationStyle: TextDecorationStyle.wavy,
+                decorationColor: Colors.red,
+
               ),
-              Align(    /// 화면을 기준으로 정렬한다. 정규화된 값으로 좌측상단 (-1,-1) 중앙(0,0)
-                alignment: Alignment(0, 0),
-                  child:Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                  )
-              ),
-              Positioned(     /// 좌측상단을 기준으로 얼마나 멀어질지 설정하는 위젯
-                top: 500,
-                left: 500,
-                child:Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.green,
-                )
-              )
-            ],
-          ),
-        )
+            ),
+            Text("data")
+          ],
+        ),
       ),
     );
   }
 }
+
 
