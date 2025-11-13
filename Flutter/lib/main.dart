a83674a5b1ec66fe54e766aebf9f7dbcc1686755
Chujer <113624740+Chujer@users.noreply.github.com>
2025-11-13 18:46:42 +0900

    2025_11_13 Stack, Align, Positioned
    Stack : 겹칠 수 있는 위젯
    Align : 정규화된 화면위치를 기준으로 요소의 위치를 설정 좌측상단(-1,-1), 중앙 (0,0)
    Positioned : 좌측상단 00 에서 얼마나 띄울지 설정하는 위젯

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -10,32 +10,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("FunCoding"),
-          centerTitle: true,
-          backgroundColor: Colors.amberAccent,
-          elevation: 10.0,
-          leading: IconButton(
-              icon: Icon(Icons.menu),
-            tooltip: 'Menu Icon',
-            onPressed: (){},
+        body: Center(
+          child: Stack(   ///Stack이란 : 겹칠 수 있는 위젯 컬럼과 로우는 겹치지않음
+            children: [
+              Container(
+                width: 400,
+                height: 400,
+                color: Colors.yellow,
+              ),
+              Align(    /// 화면을 기준으로 정렬한다. 정규화된 값으로 좌측상단 (-1,-1) 중앙(0,0)
+                alignment: Alignment(0, 0),
+                  child:Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.blue,
+                  )
+              ),
+              Positioned(     /// 좌측상단을 기준으로 얼마나 멀어질지 설정하는 위젯
+                top: 500,
+                left: 500,
+                child:Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                )
+              )
+            ],
           ),
-          actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.comment),
-            onPressed: (){},
-            tooltip: 'comment Icon',
-            ),
-            IconButton(
-              icon: Icon(Icons.settings),
-              onPressed: (){},
-              tooltip: 'Setting Icon',
-            )
-          ],
-        ),
+        )
       ),
     );
   }
 }
+
