e91b782748f0db58b1a59b2eb0774e9766b0ad1a
Chujer <113624740+Chujer@users.noreply.github.com>
2025-11-17 19:36:09 +0900

    2025.11.17 텍스트필드 위젯
    TextField위젯

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -1,81 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MusicPlayer());
+  runApp(MyApp());
 }
 
-class MusicPlayer extends StatelessWidget {
-  const MusicPlayer({super.key});
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  final TextEditingController _emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Funcoding Music Player'),
-          titleTextStyle: TextStyle(color: Colors.white),
-          backgroundColor: Colors.purple,
-          centerTitle: true,
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [Colors.purple, Colors.purpleAccent],
-            )
-          ),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/images/test.PNG'),
-                  radius: 100,
-                ),
-                SizedBox(height: 20,),
-                Text('Song Title',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white
-                  ),),
-                SizedBox(height: 10,),
-                Text('Artist Name', style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white70
-                ),),
-                SizedBox(height: 30,),
-                Slider(value: 0.5, onChanged: (value) {},
-                  activeColor: Colors.white,
-                inactiveColor: Colors.grey,),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(onPressed: () {},
-                        icon: Icon(Icons.fast_rewind),
-                        color: Colors.white,
-                        iconSize: 50,
+        body: Center(
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: TextStyle(
+                    color: Colors.amber
+                  ),
+                  hintText: 'Enter your Email',
+                  hintStyle: TextStyle(
+                    color: Colors.brown
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(
+                      width: 2, color: Colors.red
+                    ),
 
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    borderSide: BorderSide(
+                        width: 2, color: Colors.blue
                     ),
-                    IconButton(onPressed: () {},
-                        icon: Icon(Icons.play_arrow),
-                      color: Colors.white,
-                      iconSize: 50,
-                    ),
-                    IconButton(onPressed: () {},
-                        icon: Icon(Icons.fast_forward),
-                      color: Colors.white,
-                      iconSize: 50,
-                    ),
-                  ],
-                )
-              ],
-            ),
+                  ),
+                  icon: Icon(Icons.settings),
+                  prefixIcon: Icon(Icons.phone),
+                  suffixIcon: Icon(Icons.star),
+                ),
+                keyboardType: TextInputType.emailAddress, // 모바일 키보드 나오는 형태
+                textInputAction: TextInputAction.search,  //모바일에서 키보드 우측하단의 검색마크
+                obscureText: true,                        //화면에 표시안되게
+                controller: _emailController,
+              ),
+              ElevatedButton(
+                  onPressed: (){
+                    print(_emailController.text);
+                  },
+                  child: Text('Click')),
+            ],
           ),
         ),
       ),
