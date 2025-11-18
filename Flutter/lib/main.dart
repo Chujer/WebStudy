b46225cd1e56de2cf1951c859cafe4454281dc2f
Chujer <113624740+Chujer@users.noreply.github.com>
2025-11-18 18:31:43 +0900

    2025.11.18 Checkbox, Radio

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -12,23 +12,66 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int _count= 0;
+
+  bool _checkBoxValue = true;
+  String _radioValue = 'Option 1';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Center(child: ElevatedButton(
-            child: Text(
-                '$_count'
-            ),
-            onPressed: (){
-                setState(() {
-                  _count++;
-                });
-              },
-            ),
+        appBar: AppBar(
+          title: Text('Form Controls Example'),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text('$_checkBoxValue'),
+              ),
+              SizedBox(height: 16,),
+              Text('Checkbox : '),
+              Checkbox(
+                value: _checkBoxValue,
+                onChanged: (value)
+                {
+                  setState(() {
+                    _checkBoxValue = value!;
+                  });
+                },
+              ),
+              SizedBox(height: 16,),
+              Text('Radio Button'),
+              Row(
+                children: [
+                  Radio(
+                    value: 'Option 1',
+
+                    groupValue: _radioValue,
+                    onChanged: (value){
+                      setState(() {
+                        _radioValue = value.toString();
+                        print('Option1:$value');
+                      });
+                    },
+                  ),
+                  Text('Option 1'),
+                  Radio(
+                    value: 'Option 2',
+                    groupValue: _radioValue,
+                    onChanged: (value) {
+                        setState(() {
+                          _radioValue = value.toString();
+                          print('Option2:$value');
+                        });
+                      }
+                  ),
+                  Text('Option 2'),
+                ],
+              )
+            ],
           ),
         ),
       ),
