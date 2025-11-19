fe3c5f5d567959787e98f3160da399f767480cc5
Chujer <113624740+Chujer@users.noreply.github.com>
2025-11-19 19:07:32 +0900

    2025.11.19 ListView, ListTile, Card

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -12,97 +12,54 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
-  final _formkey = GlobalKey<FormState>();
-  String _name ='';
-  String _email = '';
-  String _password = '';
-  String _erroMessage = '';
-
-  void _submitForm(){
-    if(_formkey.currentState!.validate()){
-      _formkey.currentState!.save();
-      print('$_name, $_email, $_password');
-    }else{
-      setState(() {
-        _erroMessage='pleas fill in all fields';
-      });
-      }
-  }
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Form Example'),
-        ),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formkey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: 'Name'
-                ),
-                validator: (value){
-                  if(value == null || value.isEmpty)
-                  {
-                    return 'Please enter your name';
-                  }
-                  return null;
-                },
-                onSaved: (value){
-                  _name = value!;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: 'Email'
-                ),
-                validator: (value){
-                  if(value == null || value.isEmpty)
-                  {
-                    return 'Please enter your Email';
-                  }
-                  return null;
-                },
-                onSaved: (value){
-                  _email = value!;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  errorStyle: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 10
-                  ),
-                  labelText: 'Password'
-                ),
-                validator: (value){
-                  if(value == null || value.isEmpty)
-                  {
-                    return 'Please enter your Password';
-                  }
-                  return null;
-                },
-                onSaved: (value){
-                  _password = value!;
-                },
-              ),
-              ElevatedButton(
-                  onPressed: _submitForm,
-                  child: Text('Submit')
-              ),
-              Text(_erroMessage, style: TextStyle(color: Colors.red),)
-            ],
-          ),
-        ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Simple App'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
       ),
+      body: ListView.separated(
+        //physics: BouncingScrollPhysics(), // physics : 스크롤스타일 / ios 스타일 스크롤
+        //physics: NeverScrollableScrollPhysics(), // 스크롤 금지
+        //physics: ClampingScrollPhysics(), // 스크롤시 바운딩 금지
+        itemCount: 6,
+        itemBuilder: (context, position){
+          return ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.amber,
+            child: Text(position.toString()),
+          ),
+            title: Text('Item $position'),
+            subtitle: Text('Item$position description'),
+          );
+        },
+        separatorBuilder: (context, position){
+          return Card(
+            color: Colors.grey,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Separator $position',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
