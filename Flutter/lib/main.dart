69f129be3527c3541cc9ac2318e786b76bd9cf53
Chujer <113624740+Chujer@users.noreply.github.com>
2025-12-01 19:24:56 +0900

    2025.12.01 AlertDialog

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -10,172 +10,53 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent)
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('AlertDialog Sample'),
+        ),
+        body: Center(
+          child: DialogExample(),
+        ),
       ),
-      home: MyHomepage(),
     );
   }
 }
 
-class MyHomepage extends StatefulWidget {
-  const MyHomepage({super.key});
-
-  @override
-  State<MyHomepage> createState() => _MyHomepageState();
-}
-
-class _MyHomepageState extends State<MyHomepage> {
-  int _currentPage = 0;
-  final PageController _controller = PageController(viewportFraction: 0.9);
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _controller.addListener((){
-      setState(() {
-        _currentPage = _controller.page!.round();
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    _controller.dispose();
-  }
+class DialogExample extends StatelessWidget {
+  const DialogExample({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Inspirational Moments',
-          style: TextStyle(color: Colors.white
-          ),),
-        centerTitle: true,
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Theme.of(context).colorScheme.primary,
-                Theme.of(context).colorScheme.primaryContainer,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight
-            )
-          ),
-        ),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Theme.of(context).colorScheme.primary,
-              Theme.of(context).colorScheme.primaryContainer,
-            ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight
-          )
-        ),
-        child: Stack(
-          children: [
-            PageView(
-              controller: _controller,
-              children: [
-                _buildPage(
-                    imageUrl: 'assets/images/camera.png',
-                    useName: 'Dave Lee',
-                    avatarUrl: 'https://https://randomuser.me/api/portraits/men/1.jpg'
-                ),
-                _buildPage(
-                    imageUrl: 'assets/images/LodingScreen.png',
-                    useName: 'Jane Smith',
-                    avatarUrl: 'https://https://randomuser.me/api/portraits/women/1.jpg'
-                ),
-                _buildPage(
-                    imageUrl: 'assets/images/test.png',
-                    useName: 'Alex Johnson',
-                    avatarUrl: 'https://https://randomuser.me/api/portraits/men/2.jpg'
-                ),
-              ],
+    return TextButton(
+      onPressed: () => showDialog(
+          context: context,
+          barrierDismissible: true,
+          builder: (BuildContext context) => AlertDialog(
+            title: Text('AlertDialog Title'),
+            content: Text('AlertDialog description'),
+            backgroundColor: Colors.grey,
+            elevation: 24,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8)
             ),
-            Positioned(
-                bottom: 20,
-                left: 0,
-                right: 0,
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children:
-                    List.generate(3, (index) {
-                      return Container(
-                        width: 8,
-                        height: 8,
-                        margin: EdgeInsets.symmetric(horizontal: 2),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: _currentPage == index ?
-                          Theme.of(context).colorScheme.primary :
-                          Theme.of(context).colorScheme.primaryContainer
-                        ),
-                      );
-                    })
-                ))
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildPage({
-    required String imageUrl,
-    required String useName,
-    required String avatarUrl,
-}){
-    return Stack(
-      children: [
-        Positioned.fill(
-          child:ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Image.asset(
-            imageUrl,
-            fit: BoxFit.cover,
-          ),
-        )
-        ),
-        Positioned(
-          bottom: 20,
-          left: 20,
-          child: Row(
-            children: [
-              CircleAvatar(
-                backgroundImage: NetworkImage(avatarUrl),
+            contentPadding: EdgeInsets.all(50),
+            actionsPadding: EdgeInsets.all(2),
+            titlePadding: EdgeInsets.all(12),
+            buttonPadding: EdgeInsets.all(0),
+            actions: [
+              TextButton(
+                  onPressed: ()=>Navigator.of(context).pop(),
+                  child: Text('Cancel')
               ),
-              SizedBox( width: 10,),
-              Text(
-                useName,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  shadows: [
-                    Shadow(
-                      offset: Offset(1, 1),
-                      blurRadius: 2,
-                      color: Colors.black
-                    )
-                  ]
-                ),
-              )
+              TextButton(
+                  onPressed: ()=>Navigator.of(context).pop(),
+                  child: Text('OK')
+              ),
             ],
-          )  ,
-        )
-      ],
+          )
+      ),
+      child: Text('Show Dialog'),
     );
   }
 }
-
 
