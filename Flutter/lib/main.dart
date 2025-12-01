5cd96f3acc08c57894872a8647c7bf4e4ab5ffbb
Chujer <113624740+Chujer@users.noreply.github.com>
2025-12-01 18:20:25 +0900

    2025.12.01 GridView, PageView

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -4,209 +4,84 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final _formKey = GlobalKey<FormState>();
-  int _rating = 0;
-  String _successMessage = '';
-  double _sliderValue = 0.0;
-  bool _agreeToTerms = false;
-  String _name = '';
-  String _email = '';
-  String _description = '';
-
-  @override
+  @override   //모바일용
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.white,
         appBar: AppBar(
-          title: Text('Feedback Form',
-            style: TextStyle(
-              color: Colors.white
-            ),
-          ),
-          backgroundColor: Colors.deepPurple,
-          centerTitle: true,
+          title: Text('PageView Example'),
         ),
-        body: Padding(
-          padding: EdgeInsets.all(16),
-          child: Form(
-            key: _formKey,
-            child: ListView(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Name',
-                    hintText: 'Dave Lee',
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your name';
-                    }
-                    return null;
-                  },
-                  onSaved: (value){
-                    _name = value!;
-                  },
-                ),
-                SizedBox(height: 16,),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'dreem@fun-coding.org',
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your Email';
-                    }
-                    return null;
-                  },
-                  onSaved: (value){
-                    _email = value!;
-                  },
-                ),
-                SizedBox(height: 16,),
-                TextFormField(
-                  maxLines: 4,
-                  decoration: InputDecoration(
-                    labelText: 'Description',
-                    hintText: 'Tell us about your experience',
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your Email';
-                    }
-                    return null;
-                  },
-                  onSaved: (value){
-                    _description = value!;
-                  },
-                ),
-
-                SizedBox(height: 16,),
-                ListTile(
-                  title: Text("How satisfied are you with our cur service?",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18),
-                  ),
-                  trailing: Text('$_rating', style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List<Widget>.generate(5, (index) {
-                    return Radio<int>(
-                      value: index + 1,
-                      activeColor: Colors.deepPurple,
-                      groupValue: _rating,
-                      onChanged: (value) {
-                        setState(() {
-                          _rating = value!;
-                        });
-                      },
-                    );
-                  }
-                  ),
-                ),
-                ListTile(
-                  title: Text("How satisfied are you with our customer service?",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18
-                  ),
-                ),
-                  trailing: Text('${_sliderValue.toStringAsFixed(0)}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18
-                  ),
-                  ),
-                ),
-                Slider(
-                  value: _sliderValue,
-                  onChanged: (value){
-                  setState(() {
-                      _sliderValue = value;
-                    }
-                  );
-                  },
-                  min: 0,
-                  max: 10,
-                  divisions: 10,
-                  label: _sliderValue.toString(),
-                  activeColor: Colors.deepPurple,
-                ),
-                CheckboxListTile(
-                  title: Text('Subscribe to our newsletter',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                    ),
-                  ),
-                  value: _agreeToTerms,
-                  onChanged: (value){
-                    setState(() {
-                      _agreeToTerms = value!;
-                    });
-                  },
-                  activeColor: Colors.deepPurple,
-                ),
-                ListTile(
-                  title: Text("How satisfied are you with our customer service?"),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      setState(() {
-                        _formKey.currentState!.save();
-                        _successMessage = 'Submission Complete';
-                      });
-                    }
-                  },
-                  child: Text('Submit',
-                  style: TextStyle(
-                      fontSize: 16,
-                    color: Colors.white
-                  ),
-
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepPurple,
-                    padding: EdgeInsets.symmetric(vertical: 18,horizontal: 24)
-                  ),
-                ),
-                Padding(padding: EdgeInsets.all(12.0),
-                  child: Center(
-                    child: Text(_successMessage,
-                      style: TextStyle(
-                          color: Colors.blueAccent,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+        body: PageView(
+          controller: PageController(
+            initialPage: 2,
+            viewportFraction: 0.7,
           ),
+          children: [
+            Container(margin: EdgeInsets.all(10),
+              color: Colors.red,),
+            Container(margin: EdgeInsets.all(10),
+              color: Colors.blue,),
+            Container(margin: EdgeInsets.all(10),
+              color: Colors.green,),
+          ],
         ),
       ),
     );
   }
 }
+
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+//
+// class _MyAppState extends State<MyApp> {
+//   final _formKey = GlobalKey<FormState>();
+//   int _rating = 0;
+//   String _successMessage = '';
+//   double _sliderValue = 0.0;
+//   bool _agreeToTerms = false;
+//   String _name = '';
+//   String _email = '';
+//   String _description = '';
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('GridView Example'),
+//         ),
+//         body: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: GridView.builder(
+//             //scrollDirection: Axis.horizontal,
+//
+//             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//               crossAxisCount: 2,   //한줄에 몇개의 객체를 배치할 것이냐
+//               crossAxisSpacing: 8.0,  // 좌우 간격
+//               mainAxisSpacing: 36.0 //위아래 간격
+//             ),
+//             itemCount: 10,
+//             itemBuilder: (context, index){
+//               return Container(
+//                 color: Colors.blueGrey,
+//                 child: Center(
+//                   child: Text('$index'),
+//                 ),
+//               );
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 
