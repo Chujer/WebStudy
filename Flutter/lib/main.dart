5c4e91cb7b5c4971fdf6fe3928a6a3e73375c12f
Chujer <113624740+Chujer@users.noreply.github.com>
2025-12-02 18:33:13 +0900

    2025.12.02 : BottomNavigationBar

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -11,49 +11,46 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin{
-  late TabController _tabController;
+class _MyAppState extends State<MyApp> {
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _tabController = TabController(length: 3, vsync: this); //length : 탭의 개수, vsync : 해당 SingleTickerProviderStateMixin을 상속한 객체
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    _tabController.dispose();
-    super.dispose();
-  }
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TabBar Example',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('TabBar Example'),
-
-          bottom: TabBar(
-            controller: _tabController,
-            tabs: <Widget>[
-              Tab(icon : Icon(Icons.home), text: 'Home'),
-              Tab(icon : Icon(Icons.star), text: 'Favorites'),
-              Tab(icon : Icon(Icons.settings), text: 'Settings'),
-            ],
-          ),
-        ),
-        body: TabBarView(       //탭이 달라질때 바꿀 본문
-          controller: _tabController,
-          children: <Widget>[
-            Center(child: Text('Home'),),
-            Center(child: Text('Favorites'),),
-            Center(child: Text('Settings'),),
-
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                activeIcon: Icon(Icons.alarm, color:Colors.blue), // 활성화 한 아이콘 표시
+                label: '홈'
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.favorite),
+                label: '즐겨찾기'
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: '설정'
+            ),
           ],
+          onTap: (index){
+            print(index);
+            setState(() {
+              _currentIndex = index;
+            });
+
+          },
+          currentIndex: _currentIndex,
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.brown,
+          unselectedItemColor: Colors.blueAccent,
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
         ),
+        backgroundColor: Colors.amber ,
       ),
     );
   }
