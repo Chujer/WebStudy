dbde433adaae20eeded2112e5fbd66fef4b5d440
Chujer <113624740+Chujer@users.noreply.github.com>
2025-12-02 18:48:33 +0900

    2025.12.02 GestureDetector onVerticalDragStart, onVerticalDragUpdate,

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -4,54 +4,97 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage(),);
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-
-  int _currentIndex = 0;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                activeIcon: Icon(Icons.alarm, color:Colors.blue), // 활성화 한 아이콘 표시
-                label: '홈'
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.favorite),
-                label: '즐겨찾기'
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.settings),
-                label: '설정'
-            ),
-          ],
-          onTap: (index){
-            print(index);
-            setState(() {
-              _currentIndex = index;
-            });
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-          },
-          currentIndex: _currentIndex,
-          backgroundColor: Colors.white,
-          selectedItemColor: Colors.brown,
-          unselectedItemColor: Colors.blueAccent,
-          type: BottomNavigationBarType.fixed,
-          showSelectedLabels: true,
-          showUnselectedLabels: true,
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  double _top = 0;
+  String _dragStatus = 'Drag the text verticall';
+
+  void _onVerticalDragStart(DragStartDetails details){
+    setState(() {
+      _dragStatus = 'Vertical drag started';
+      _top = details.globalPosition.dy;
+    });
+  }
+  void _onVerticalDragUpdate(DragUpdateDetails details){
+    setState(() {
+      _top += details.delta.dy;
+
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onVerticalDragStart: _onVerticalDragStart,
+        onVerticalDragUpdate: _onVerticalDragUpdate,
+        child: Container(
+          child: Stack(
+            children: [
+              Positioned(
+                top: _top,
+                child: Text(_dragStatus),
+              )
+            ],
+          )
         ),
-        backgroundColor: Colors.amber ,
       ),
     );
   }
 }
+
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Gesture Demo',
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Gesture Demo'),
+//       ),
+//       body: GestureDetector(
+//         onTap: (){
+//           print('Screen tapped');
+//         },
+//         child: Container(
+//           color: Colors.white,
+//           child: Center(
+//             child: Text(
+//               'Tap anywhere on the screen',
+//               style: TextStyle(fontSize: 24),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
