cf944f3380d3aae1ffb8196796c7e231c9c72981
Chujer <113624740+Chujer@users.noreply.github.com>
2025-12-02 18:54:03 +0900

    2025.12.02 각종버튼(IconButton, ElevateButton, FloatinActionButton)

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -9,54 +9,91 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage(),);
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  double _top = 0;
-  String _dragStatus = 'Drag the text verticall';
-
-  void _onVerticalDragStart(DragStartDetails details){
-    setState(() {
-      _dragStatus = 'Vertical drag started';
-      _top = details.globalPosition.dy;
-    });
-  }
-  void _onVerticalDragUpdate(DragUpdateDetails details){
-    setState(() {
-      _top += details.delta.dy;
-
-    });
-  }
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GestureDetector(
-        onVerticalDragStart: _onVerticalDragStart,
-        onVerticalDragUpdate: _onVerticalDragUpdate,
-        child: Container(
-          child: Stack(
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Button Example'),),
+        body: Center(
+          child: Column(
             children: [
-              Positioned(
-                top: _top,
-                child: Text(_dragStatus),
-              )
+              IconButton(
+                onPressed: (){
+                  print('IconButton');
+                },
+                icon: Icon(Icons.favorite),
+              ),
+              ElevatedButton(
+                  onPressed: (){
+                print('ElevateButton');
+              }, 
+                  child: Text('ElevatedButton')),
             ],
-          )
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            print('FloatingActionButton');
+          },
+          child: Icon(Icons.add),
         ),
       ),
     );
   }
 }
+
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(home: MyHomePage(),);
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key});
+//
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   double _top = 0;
+//   String _dragStatus = 'Drag the text verticall';
+//
+//   void _onVerticalDragStart(DragStartDetails details){
+//     setState(() {
+//       _dragStatus = 'Vertical drag started';
+//       _top = details.globalPosition.dy;
+//     });
+//   }
+//   void _onVerticalDragUpdate(DragUpdateDetails details){
+//     setState(() {
+//       _top += details.delta.dy;
+//
+//     });
+//   }
+//
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: GestureDetector(
+//         onVerticalDragStart: _onVerticalDragStart,
+//         onVerticalDragUpdate: _onVerticalDragUpdate,
+//         child: Container(
+//           child: Stack(
+//             children: [
+//               Positioned(
+//                 top: _top,
+//                 child: Text(_dragStatus),
+//               )
+//             ],
+//           )
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 
 // class MyApp extends StatelessWidget {
