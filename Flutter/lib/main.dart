85a7a3c67aaab8f93256e31509623575cd935c13
Chujer <113624740+Chujer@users.noreply.github.com>
2025-12-04 19:23:14 +0900

    Revert "2025.12.04 MultiProvider"

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -1,24 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:firstproject/new_page.dart';
+import 'routes.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-class PlusCounter with ChangeNotifier{
-  int count = 0;
-  void increase(){
-    count++;
-    notifyListeners();
-  }
-}
-
-class MinusCounter with ChangeNotifier{
-  int count = 100;
-  void decrease(){
-    count--;
-    notifyListeners();
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -27,62 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Provider Test'),
-        ),
-        body: MultiProvider(
-          providers: [
-            ChangeNotifierProvider(
-                create: (context) =>
-                    PlusCounter()
-            ),
-            ChangeNotifierProvider(
-                create: (context) =>
-                    MinusCounter()
-            )
-          ],
-          child: MyWidget(),
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
       ),
+      initialRoute: '/home',
+      routes: routes,
     );
   }
 }
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final plus = Provider.of<PlusCounter>(context);
-    final minus = Provider.of<MinusCounter>(context);
-    return Center(
-      child: Column(
-        children: [
-          SizedBox(height: 20,),
-          Text('Plus : ${plus.count}'),
-          SizedBox(height: 20,),
-          Text('Minus : ${minus.count}'),
-          SizedBox(height: 20,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                  onPressed: (){
-                    plus.increase();
-                  },
-                  child: Text('+')
-              ),
-              ElevatedButton(
-                  onPressed: (){
-                    minus.decrease();
-                  },
-                  child: Text('-')
-              ),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
